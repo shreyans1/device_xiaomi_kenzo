b23a340f195eb2d2cdc35a469e951e0795f71f99
shreyans1 <shreyans.jain.cer14@iitbhu.ac.in>
2016-09-04 20:31:53 +0530

    Update aosip.mk

--- aosip.mk
+++ aosip.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Cardinal-AOSP stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+$(call inherit-product, vendor/aosip/common.mk)
 
 # Inherit from kenzo device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
